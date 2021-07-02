b1b83b6df8f91a2bc838d5baa5e84927e9b5cfe5
Paul Chin Jr <pchinjr@gmail.com>
2021-07-02 13:04:33 -0400

    end to end tests for user sign up and getmyprofile query

--- __tests__/steps/when.js
+++ __tests__/steps/when.js
@@ -3,6 +3,8 @@ const AWS = require('aws-sdk')
 const fs = require('fs')
 const velocityMapper = require('amplify-appsync-simulator/lib/velocity/value-mapper/mapper')
 const velocityTemplate = require('amplify-velocity-template')
+const GraphQL = require('../lib/graphql')
+
 const we_invoke_confirmUserSignup = async (username, name, email) => {
   const handler = require('../../functions/confirm-user-signup').handler
 
@@ -40,7 +42,7 @@ const a_user_signs_up = async (password, name, email) => {
     Username: email,
     Password: password,
     UserAttributes: [
-      { Name: 'name', Value: name}
+      { Name: 'name', Value: name }
     ]
   }).promise()
 
@@ -61,8 +63,8 @@ const a_user_signs_up = async (password, name, email) => {
   }
 }
 
-const we_invoke_an_appsync_template = (templatePath, context ) => {
-  const template = fs.readFileSync(templatePath, { encoding: 'utf-8'})
+const we_invoke_an_appsync_template = (templatePath, context) => {
+  const template = fs.readFileSync(templatePath, { encoding: 'utf-8' })
   const ast = velocityTemplate.parse(template)
   const compiler = new velocityTemplate.Compile(ast, {
     valueMapper: velocityMapper.map,
@@ -70,8 +72,38 @@ const we_invoke_an_appsync_template = (templatePath, context ) => {
   })
   return JSON.parse(compiler.render(context))
 }
+
+const a_user_calls_getMyProfile = async (user) => {
+  const getMyProfile = `query getMyProfile {
+    getMyProfile {
+      backgroundImageUrl
+      bio
+      birthdate
+      createdAt
+      followersCount
+      followingCount
+      id
+      imageUrl
+      likesCounts
+      location
+      name
+      screenName
+      tweetsCount
+      website
+    }
+  }`
+
+  const data = await GraphQL(process.env.API_URL, getMyProfile, {}, user.accessToken)
+  const profile = data.getMyProfile
+
+  console.log(`[${user.username}] - fetched profile`)
+
+  return profile
+}
+
 module.exports = {
   we_invoke_confirmUserSignup,
   a_user_signs_up,
-  we_invoke_an_appsync_template
+  we_invoke_an_appsync_template,
+  a_user_calls_getMyProfile
 }
